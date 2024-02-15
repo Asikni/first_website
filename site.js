c8a78c3709e860c48253d914f99576ee525a8671
Imran <mohdimranshountoo@gmail.com>
2024-02-15 14:54:57 +0530

    updated code

---
+++ site.js
@@ -0,0 +1,11 @@
+function openNav() {
+   
+    document.getElementById("mySidenav").style.marginLeft = "0px";
+    document.getElementById("main").style.marginLeft = "0px";
+
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.marginLeft = "-250px";
+    document.getElementById("main").style.marginLeft= "0";
+  }
