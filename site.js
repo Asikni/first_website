106ba82e2ff17bcaa049fb693cd5c905a7368f47
Imran <mohdimranshountoo@gmail.com>
2024-02-16 12:50:51 +0530

    updated code

--- site.js
+++ site.js
@@ -9,3 +9,15 @@ function openNav() {
     document.getElementById("mySidenav").style.marginLeft = "-100vw";
     document.getElementById("main").style.marginLeft= "0vw";
   }
+
+  document.getElementById('searchIcon').onclick = function() {
+    document.getElementById('search').classList.add("visible");
+    document.getElementById('clear').classList.add("visible");
+    document.getElementById('search').focus();
+    document.getElementById('searchIcon').classList.add("hide");
+  }
+  document.getElementById('clear').onclick = function() {
+    document.getElementById('searchIcon').classList.remove("hide");
+    document.getElementById('search').classList.remove("visible");
+    document.getElementById('clear').classList.remove("visible");
+  }
