1406269c60f4ad1b3b3a6dc8a02cb7e235e987af
Imran <mohdimranshountoo@gmail.com>
2024-02-18 22:25:21 +0530

    updated code

--- site.js
+++ site.js
@@ -10,14 +10,12 @@ function openNav() {
     document.getElementById("main").style.marginLeft= "0vw";
   }
 
-  document.getElementById('searchIcon').onclick = function() {
-    document.getElementById('search').classList.add("visible");
-    document.getElementById('clear').classList.add("visible");
-    document.getElementById('search').focus();
-    document.getElementById('searchIcon').classList.add("hide");
-  }
-  document.getElementById('clear').onclick = function() {
-    document.getElementById('searchIcon').classList.remove("hide");
-    document.getElementById('search').classList.remove("visible");
-    document.getElementById('clear').classList.remove("visible");
-  }
+const input = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-btn");
+
+const expand = () => {
+  searchBtn.classList.toggle("close");
+  input.classList.toggle("square");
+};
+
+searchBtn.addEventListener("click", expand);
