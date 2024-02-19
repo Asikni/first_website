884543b64143d458dbb061e303af9b18de99eb0e
Imran <mohdimranshountoo@gmail.com>
2024-02-19 16:49:02 +0530

    updated code

--- site.js
+++ site.js
@@ -19,26 +19,66 @@ const expand = () => {
 };
 
 searchBtn.addEventListener("click", expand);
+
+
 let state=false;
+function clickBell(){
+  // state=!state;
+  console.log(state)
+  let notification = document.getElementById("notification-icon");
+
+
+
 
 if (state===false){
-function clickBell(){
-  
-  let notification = document.getElementById("notification-icon")
   notification.innerHTML="Subscribed"
 
-  const width_1 = document.getElementById("notification");
-  width_1.style.width="10px";
-  state=true;
-}
+  state=true
 } else{
-  function clickBell(){
+  notification.innerHTML="Subscribe for notifications";
 
-  let notification = document.getElementById("notification-icon")
-  notification.innerHTML="Subscribe for notifications"
-
-  const width_1 = document.getElementById("notification");
-  width_1.style.width="50px";
   state=false;
 }
+}
+
+
+
+const key = '50c2bc48e2ae5b37bf4637a5d16d2377';
+if(key=='') document.getElementById('temp').innerHTML = ('Remember to add your api key from openweathermap.org/api');
+
+
+
+function weatherBallon( cityID ) {
+	fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID+ '&appid=' + key)  
+	.then(function(resp) { return resp.json() }) // Convert data to json
+	.then(function(data) {
+    
+    console.log(data);
+		drawWeather(data);
+	})
+	.catch(function() {
+		// catch any errors
+	});
+}
+function drawWeather( d ) {
+  var celcius = Math.round(parseFloat(d.main.temp)-273.15);
+	var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32);
+  var description = d.weather[0].description; 
+	
+	document.getElementById('description').innerHTML = description;
+	document.getElementById('temp').innerHTML = celcius + '&deg;';
+	document.getElementById('location').innerHTML = d.name;
+  
+  if( description.indexOf('rain') > 0 ) {
+  	document.body.className = 'rainy';
+  } else if( description.indexOf('cloud') > 0 ) {
+  	document.body.className = 'cloudy';
+  } else if( description.indexOf('sunny') > 0 ) {
+  	document.body.className = 'sunny';
+  } else {
+  	document.body.className = 'clear';
+  }
+}
+window.onload = function() {
+	weatherBallon( 1255634 );
 }
