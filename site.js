70403a8b6a5b5ddb1db8aebba7655e92aa520310
Imran <mohdimranshountoo@gmail.com>
2024-02-19 14:31:14 +0530

    updated code

--- site.js
+++ site.js
@@ -19,3 +19,26 @@ const expand = () => {
 };
 
 searchBtn.addEventListener("click", expand);
+let state=false;
+
+if (state===false){
+function clickBell(){
+  
+  let notification = document.getElementById("notification-icon")
+  notification.innerHTML="Subscribed"
+
+  const width_1 = document.getElementById("notification");
+  width_1.style.width="10px";
+  state=true;
+}
+} else{
+  function clickBell(){
+
+  let notification = document.getElementById("notification-icon")
+  notification.innerHTML="Subscribe for notifications"
+
+  const width_1 = document.getElementById("notification");
+  width_1.style.width="50px";
+  state=false;
+}
+}
